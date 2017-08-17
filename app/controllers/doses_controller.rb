ffbae0d362388e0870d5df0765e65ea381e41d56
Kilian Martin <kilian.martin@me.com>
2017-08-17 16:26:53 +0200

    backend almost done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    # @ingredient = Ingredient.find(params[:ingredient_id])
+    # @dose.ingredient = @ingredient
+
+
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+require 'json'
+puts "Destroy"
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+puts "create new DB"
+ingredients = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+ingredients['drinks'].each do |i|
+  Ingredient.create(name: i['strIngredient1'])
+end
+# Ingredient.create(name: "lemon")
+cocktail = Cocktail.new(name: "Mojito")
+dose = Dose.new(description: "10cl")
+dose.cocktail = cocktail
+dose.ingredient = Ingredient.all.sample
+dose.save
+dose.ingredient.save
+cocktail.save
