cd2ab73cf6e24e6865d1a0928d82d3dc10830a92
Kilian Martin <kilian.martin@me.com>
2017-08-18 16:42:43 +0200

    added root to cocktails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+
   resources :cocktails , only: [ :index, :create, :new, :show ] do
     resources :doses , only: [ :create, :new ]
   end
